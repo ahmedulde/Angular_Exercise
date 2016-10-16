6eb7dea57b43c0279cf6b6ea7a7705f981c6170b
ahmedulde <ahmed.ulde@mavs.uta.edu>
2016-10-16 15:21:34 -0500

    fix for grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.export = function(grunt){
+module.exports = function(grunt){
 
 	// Time how long tasks take. Can help when optimizing build times
 	require('time-grunt')(grunt);
